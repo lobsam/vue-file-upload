d22ebf4475be754885ad7555f674df3e96c36790
lobsam <sanduplobzang@gmail.com>
2023-06-02 22:15:38 +0530

    adding dist

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue-file-upload/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
