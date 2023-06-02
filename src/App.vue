9e21f72855d66150b6fe95e33a4c528d731fc57f
lobsam <sanduplobzang@gmail.com>
2023-06-03 00:28:56 +0530

    adding dist

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-
-<template>
-  <main>
-    <router-link to="/" exact>Home</router-link> |
-    <router-link to="/organization">organization</router-link>
-    <router-view />
-  </main>
-</template>
-
--- src/headerSession/headers.js
+++
@@ -1,8 +0,0 @@
-
-export const headers = {
-    'X-Hasura-Admin-Secret': "IemdrYcQQHC0VTVk6NYfegBJV0uAftCgousB2ZuevL0QhkCwFbZ6YiCvGCy96WQO",
-    'x-hasura-user-id': '7d2aacd6-a66e-4263-9e9d-0e1e9be2c312',
-    'x-hasura-role': 'user',
-    'Content-Type': 'application/json'
-} 
-
